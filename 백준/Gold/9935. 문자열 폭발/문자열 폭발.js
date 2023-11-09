a497a386fa818f1c6fa4276140919734c4ef87da
Junho Jung <111482387+Lurang@users.noreply.github.com>
2023-11-09 18:28:05 +0900

    [Gold IV] Title: 문자열 폭발, Time: 340 ms, Memory: 78696 KB -BaekjoonHub

--- 백준/Gold/9935. 문자열 폭발/문자열 폭발.js
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.js
@@ -3,13 +3,10 @@ let str = inputs.shift();
 const bomb = inputs.shift();
 const stack = [];
 
-let pointer = 0;
-
 for (let i = 0; i < str.length; i ++) {
     stack.push(str[i]);
-    pointer = str[i];
 
-    if (pointer === bomb[bomb.length - 1] && stack.slice(-bomb.length).join('') === bomb) {
+    if (str[i] === bomb[bomb.length - 1] && stack.slice(-bomb.length).join('') === bomb) {
         stack.splice(-bomb.length);
     }
 }
