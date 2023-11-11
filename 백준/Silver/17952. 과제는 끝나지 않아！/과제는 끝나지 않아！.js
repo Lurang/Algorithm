c15e72b8e5f550507dbd32054bf12ca702728a40
Junho Jung <111482387+Lurang@users.noreply.github.com>
2023-11-11 19:19:31 +0900

    [Silver III] Title: 과제는 끝나지 않아!, Time: 880 ms, Memory: 117408 KB -BaekjoonHub

--- 백준/Silver/17952. 과제는 끝나지 않아！/과제는 끝나지 않아！.js
+++ 백준/Silver/17952. 과제는 끝나지 않아！/과제는 끝나지 않아！.js
@@ -2,22 +2,25 @@ const inputs = require('fs').readFileSync('/dev/stdin').toString().trim().split(
 
 const length = +inputs.shift();
 
-const stack = [];
-let score = 0;
+const score = [];
+const time = [];
+let total = 0;
 
 for (let i = 0; i < length; i++) {
     if (+inputs[i] !== 0) {
-        const hw = inputs[i].split(' ').map(Number);
-        stack.push(hw);
+        const [_, s, t] = inputs[i].split(' ').map(Number);
+        score.push(s);
+        time.push(t);
     }
 
-    if (stack.length) {
-        stack[stack.length - 1][2] = stack[stack.length - 1][2] - 1;
-        if (stack[stack.length - 1][2] === 0) {
-            score += stack[stack.length - 1][1];
-            stack.pop();
+    if (time.length) {
+        if (time[time.length - 1] === 1) {
+            total += score.pop();
+            time.pop();
+        } else {
+            time[time.length - 1] = time[time.length - 1] - 1;
         }
     }
 }
 
-console.log(score);
+console.log(total);
